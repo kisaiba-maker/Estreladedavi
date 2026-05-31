3ee8f661c755e0ad2237177ac1cf8fc4b11fcebd
Luiz Felipe <gudikutnis@gmail.com>
2026-05-31 14:24:59 -0300

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
 
 const poppins = Poppins({ subsets: ['latin'], weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"] })
@@ -15,7 +16,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="pt-BR">
             <body className={`${poppins.className} antialiased overflow-x-hidden`}>
                 {children}
             </body>
