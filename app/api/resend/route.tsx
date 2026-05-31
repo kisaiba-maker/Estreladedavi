59bc4f72084e3350824e8eaaa37498b90f569bcd
Luiz Felipe <gudikutnis@gmail.com>
2026-05-31 17:47:55 -0300

    email 2

--- app/api/resend/route.tsx
+++ app/api/resend/route.tsx
@@ -7,7 +7,7 @@ export async function POST(req: Request) {
     const body = await req.json();
     try {
         const data = await resend.emails.send({
-           from: 'Contato <estreladedaviprotecaoveicular@gmail.com>',
+           from: 'Contato <onboarding@resend.dev>',
             to: ['lipeetxo@Outlook.com'],
             subject: 'Estrela de Davi',
             react: EmailTemplate(body),
