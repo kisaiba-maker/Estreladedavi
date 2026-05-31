c97c4e43d605de0dc25addf727c2cca33c401296
Luiz Felipe <gudikutnis@gmail.com>
2026-05-31 18:16:02 -0300

    coisa linda

--- app/api/resend/route.tsx
+++ app/api/resend/route.tsx
@@ -8,7 +8,7 @@ export async function POST(req: Request) {
     try {
         const data = await resend.emails.send({
            from: 'Contato <contato@estreladedaviprotecao.com.br>',
-            to: ['lipeetxo@Outlook.com'],
+            to: ['estreladedavi.protecaoveicular@gmail.com],
             subject: 'Estrela de Davi',
             react: EmailTemplate(body),
         });
